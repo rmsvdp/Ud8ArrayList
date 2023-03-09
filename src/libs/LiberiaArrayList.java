ab7f60cce5968042b91c338bd4cb8a8a4c80b920
rmsvdp <rmsvdp@gmail.com>
2023-03-09 16:50:01 +0100

    rc 01

--- src/libs/LiberiaArrayList.java
+++ src/libs/LiberiaArrayList.java
@@ -3,13 +3,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+/**
+ * Librería que implementa varios métodos útiles para trabajar con
+ * datos de tipo Integer.
+ * Se incorpora una clase sencilla que implementa un conjunto de
+ * pruebas unitarias en Junit5
+ * @author rmsvdp
+ *
+ */
 public class LiberiaArrayList {
 
+	/**
+	 * Constructor para instanciar un objeto y poder acceder a los métodos.
+	 * Esta clase no dispone de atributos
+	 */
 	public LiberiaArrayList() {
 		// TODO Auto-generated constructor stub
 	}
 
 	
+	/**
+	 * Generación de un conjunto aleatorio de valores de tipo Integer
+	 * @param num Número de elmentos que se generarán
+	 * @param max Máximo valor entero que se podrá generar
+	 * @param min Mínimo valor entero que se podrá generar
+	 * @return ArrayList de enteros
+	 */
 	public ArrayList<Integer> generaArrayListInt(int num,int max,int min){
 		
 		ArrayList<Integer> miAl = new ArrayList<Integer>();
@@ -21,6 +40,11 @@ public class LiberiaArrayList {
 		return miAl;
 	}
 	
+	/**
+	 * Devuelve el mínimo entero existente en el array que se le pasa por parámetro
+	 * @param miAl ArrayList a analizar
+	 * @return valor mínimo
+	 */
 	public Integer minimoArrayListInt(ArrayList<Integer> miAl){
 		
 		Integer min=0;
@@ -33,6 +57,11 @@ public class LiberiaArrayList {
 		   return min;
 	}
 	
+	/**
+	 * Devuelve el máximo entero existente en el array que se le pasa por parámetro
+	 * @param miAl ArrayList a analizar
+	 * @return valor máximo
+	 */
 	public 	Integer maximoArrayListInt(ArrayList<Integer> miAl){
 		
 		Integer max=0;		
@@ -47,6 +76,12 @@ public class LiberiaArrayList {
 		 return max;
 	}
 	
+	/**
+	 * Devuelve la media calculada sobreel array que se le pasa por parámetro
+	 * @param miAl ArrayList a analizar
+	 * @return Media numérica (doble) calculada
+	 */
+	
 	public double mediaArrayListInt(ArrayList<Integer> miAl){
 		
 		double med=0.0;		
@@ -58,12 +93,27 @@ public class LiberiaArrayList {
 		
 }
 
+	/**
+	 * Comprueba si el número facilitado se encuentra en el array
+	 * @param miAl ArrayList a analizar
+	 * @param num Número a buscar
+	 * @return True: El número existe 1 o más veces
+	 * 		   False: número no encontrado
+	 */
+	
 public boolean estaEnArrayListInt(ArrayList<Integer> miAl,int num){
 	
 	return miAl.contains(num);
 	
 }
 
+/**
+ * Devuelve la posición en la que se encuentra el número facilitado dentro del array
+ * @param miAl ArrayList a analizar
+ * @param num Número a buscar
+ * @return posición si encontrado:  0 <-> miAl.size()-1 
+ * 		      -1 si no encontrado
+ */
 public Integer posicionEnArrayListInt(ArrayList<Integer> miAl,Integer num){
 	
 	Integer pos=0;		
@@ -71,7 +121,11 @@ public Integer posicionEnArrayListInt(ArrayList<Integer> miAl,Integer num){
 	else { pos = -1;}
 	return pos;
 }
-
+/**
+ * Devuelve una copia espejo del array facilitado (que no se ve alterado)
+ * @param miAl ArrayList a espejar
+ * @return arrayListEspejo
+ */
 public ArrayList<Integer> volteaArrayListInt(ArrayList<Integer> miAl){
 	
 	ArrayList<Integer> tmpList = new ArrayList<Integer>();
@@ -81,6 +135,11 @@ public ArrayList<Integer> volteaArrayListInt(ArrayList<Integer> miAl){
 	return tmpList;
 }
 
+/**
+ * Muestra tabularmente los contenidos del array en el formato:
+ *  posición <n> : < Valor posición>
+ * @param aL Array a mostrar
+ */
 public void muestraArrayInt(ArrayList<Integer> aL) {
 	
 	int i=0;
